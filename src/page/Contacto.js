1af73e2dcc6597e6a6a6e72170166ef3c2ef54b7
vizmarvizcaino <gatome2000@hotmail.com>
2022-08-29 11:07:19 -0500

    reac_todos

---
+++ src/page/Contacto.js
@@ -0,0 +1,9 @@
+import '../stylesheet/contacto.css';
+function Contacto () {
+    return (
+        <div className="contacto">
+            <h1>PENDIENTE POR DESARROLLAR (PAGINA EN CONSTRUCCION)</h1>
+        </div>
+    )
+}
+export default Contacto;

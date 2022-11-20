c40b2850a08261f2c6f991e32b151f68e48963c6
vizmarvizcaino <gatome2000@hotmail.com>
2022-11-20 13:14:51 -0500

    fix bug linting

--- src/components/DataAcademic/DataAcademic.js
+++ src/components/DataAcademic/DataAcademic.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import React, {useState} from 'react';
 import { useDispatch } from 'react-redux';
 import { addAcademic } from '../../features/academic/academicSlice';
 import { v4 as uuid } from 'uuid';
